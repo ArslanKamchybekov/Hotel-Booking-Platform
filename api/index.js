126f0d48a336b4d4328f85364a1c8b1c6d0c92c3
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-04-19 17:13:35 -0500

    user registration

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
-//import authRoute from "./routes/auth.js"
+import authRoute from "./routes/auth.js"
 //import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 //import roomsRoute from "./routes/rooms.js"
@@ -20,7 +20,7 @@ const connect = async()=>{
 
 //middlewares
 app.use(express.json())
-//app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoute);
 //app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 //app.use("/api/rooms", roomsRoute);
