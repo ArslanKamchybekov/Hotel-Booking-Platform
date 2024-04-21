0319c6d9e1b3d116c9d889257df7a3e69bf2ba04
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-04-21 13:38:09 -0500

    authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { register } from "../controllers/auth.js";
+import { login, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
 //Register 
 router.post("/register", register)
+
+//Login
+router.post("/login", login)
 
 export default router
