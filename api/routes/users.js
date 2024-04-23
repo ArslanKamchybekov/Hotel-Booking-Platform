c65b7730689575f2699177a7d788ad2e32395721
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-04-22 22:07:37 -0500

    JWT admin cookie

---
+++ api/routes/users.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/User.js";
+
+const router = express.Router();
+
+//Update
+router.put("/:id", updateUser)
+
+//Delete
+router.delete("/:id", deleteUser)
+
+//Get
+router.get("/:id", getUser)
+
+//Get All
+router.get("/", getUsers)
+
+export default router
