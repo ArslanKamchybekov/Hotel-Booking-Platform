9ca67d86f4318208e8aab55217fadf9938a34d6f
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-05-06 23:16:52 -0500

    verify user

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/users.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/checkauthentication", verifyToken, (req, res, next)=>{
     res.send("Login completed successfully")
+})
+
+router.get("/checkuser/:id", verifyUser, (req, res, next)=>{
+    res.send("Login completed successfully, you can delete your account")
 })
 
 //Update
